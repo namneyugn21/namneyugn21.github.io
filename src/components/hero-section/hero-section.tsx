eb3916989920a5edac626f9f780a4cf77a6900fd
nam nguyen <nguyenvuhainamhan@gmail.com>
2024-08-11 18:45:53 -0700

    complete the project section

--- src/components/hero-section/hero-section.tsx
+++ src/components/hero-section/hero-section.tsx
@@ -26,7 +26,7 @@ const HeroSection = () => {
             <div className="introduction_description">
                 <p className="roboto-slab-light">Xin chào ! (that's "Hello" in Vietnamese)</p>
                 <p className="roboto-slab-light">My name is Nam, and I am a computer science student at Simon Fraser University in Burnaby, British Columbia.</p>
-                <p className="roboto-slab-light">With a concentration in Information Systems and Programming Languages, I am passionate about web development and databases. I am currently seeking opportunities as a front-end developer or web developer.</p>
+                <p className="roboto-slab-light">With a concentration in Information Systems and Programming Languages, I am passionate about web development and human-centered computing. I am currently seeking opportunities as a front-end developer or web developer.</p>
                 <p className="roboto-slab-light">Outside of classroom, I enjoy traveling and taking aesthetic pictures around the city of Vancouver!</p>
             </div>
             <div className="introduction_menu">
--- src/components/project-section/project-section.tsx
+++ src/components/project-section/project-section.tsx
@@ -4,6 +4,9 @@ import { Typewriter } from "react-simple-typewriter";
 import ProjectCards from "../ui/project-cards/project-cards.tsx";
 
 const Realtorest = require("../../assets/Realtorest.png");
+const Petinder = require("../../assets/Petinder.png");
+const Portfolio = require("../../assets/Portfolio.png");
+const TokimonWorld = require("../../assets/TokimonWorld.png");
 
 const ProjectSection = () => {
     return (
@@ -27,9 +30,17 @@ const ProjectSection = () => {
             <div className="projects_line"></div>
             <div className="projects_tuple">
                 <div className="projects_tuple-index">
-                    <ProjectCards images={Realtorest} title="Realtorest" description="A full-stack web application designed for hosting a personal website for realtors. Exploration, sorting, and direct contact functionalities are available for visiting users, while enabling Admin to manage listings and engage with potential buyers effectively, all within a user-friendly, ad-free environment." link="https://github.com/namneyugn21/Realtorest.git" />
+                    <ProjectCards images={Petinder} title="Petinder" description="Petinder is an innovative web application designed to help animal lovers connect with pets available for adoption at local shelters. Drawing inspiration from the popular dating app Tinder, Petinder offers a user-friendly interface that allows potential pet adopters to discover, explore, and swipe through profiles of adoptable pets." link="https://github.com/namneyugn21/Petinder.git" />
                 </div>
-
+                <div className="projects_tuple-index">
+                    <ProjectCards images={Realtorest} title="Realtorest" description="A full-stack web application designed for hosting a personal website for realtors. Exploration, sorting, and direct contact functionalities are available for visiting users, while enabling Admin to manage listings and engage with potential buyers effectively, all within a user-friendly, ad-free environment." link="https://github.com/namneyugn21/Realtorest.git" /> 
+                </div>
+                <div className="projects_tuple-index">
+                    <ProjectCards images={Portfolio} title="Portfolio Website" description="As part of my journey to self-learn React, I'm focusing on designing my website with a minimalistic and aesthetically pleasing approach. My process involves exploring and incorporating various UI components and libraries available online to enhance both functionality and visual appeal." link="https://github.com/namneyugn21/namneyugn21.github.io.git" /> 
+                </div>
+                <div className="projects_tuple-index">
+                    <ProjectCards images={TokimonWorld} title="Tokimon World" description="Tokimon World is a Spring Boot application that allows users to manage Tokimon cards through a user-friendly JavaFX interface. The application supports full CRUD operations and interacts seamlessly with RESTful APIs, offering a smooth and engaging experience for both data management and user interaction." link="https://github.com/namneyugn21/TokimonWorld.git" /> 
+                </div>
             </div>
         </div>
     );

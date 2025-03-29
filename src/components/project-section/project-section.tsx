0d2be3f3aa5242ac8d60b0aa26816a367575b3b2
nam nguyen <nguyenvuhainamhan@gmail.com>
2025-03-28 19:45:35 -0700

    update project and skills sections with responsive design adjustments and new project addition

--- src/components/project-section/project-section.tsx
+++ src/components/project-section/project-section.tsx
@@ -8,6 +8,7 @@ const MySort = require("../../assets/MySort.png");
 const Portfolio = require("../../assets/Portfolio.png");
 const TokimonWorld = require("../../assets/TokimonWorld.png");
 const BMPViewer = require("../../assets/BMPViewer.png");
+const RoamingApp = require("../../assets/RoamingApp.png");
 
 const ProjectSection = () => {
     return (
@@ -16,7 +17,7 @@ const ProjectSection = () => {
                 <p className="projects_title roboto-slab-light">Project</p>
                 <p className="projects_title-comments roboto-slab-light">                    
                     <Typewriter 
-                    words={['// web applications', '// software development', '// user experience', '// problem solving', '// creativity']}
+                    words={['// web applications', '// mobile applications', '// software development', '// user experience', '// problem solving', '// creativity']}
                     loop={true}
                     cursor
                     cursorStyle='_'
@@ -29,7 +30,10 @@ const ProjectSection = () => {
             <div className="projects_line"></div>
             <div className="projects_tuple">
                 <div className="projects_tuple-index">
-                    <ProjectCards images={BMPViewer} title="BMP Image Viewer" description="BMP Image Editor is a Python GUI application for viewing and modifying BMP images. It allows users to adjust brightness, scale images with nearest-neighbor interpolation, and toggle RGB channels—all within a simple Tkinter interface." link="https://github.com/namneyugn21" />
+                    <ProjectCards images={RoamingApp} title="Roaming App" description="Roaming is a visual storytelling app that encourages users to capture raw, aesthetic moments and create a digital footprint of their experiences. Unlike traditional photo-sharing platforms that emphasize selfies and social engagement, Roaming is designed to document places, landscapes, cityscapes, and nature, allowing users to tell stories through their surroundings rather than themselves." link="https://github.com/namneyugn21/Roaming-App" />
+                </div>
+                <div className="projects_tuple-index">
+                    <ProjectCards images={BMPViewer} title="BMP Image Viewer" description="BMP Image Editor is a Python GUI application for viewing and modifying BMP images. It allows users to adjust brightness, scale images with nearest-neighbor interpolation, and toggle RGB channels—all within a simple Tkinter interface." link="https://github.com/namneyugn21/BmpImageViewer" />
                 </div>
                 <div className="projects_tuple-index">
                     <ProjectCards images={MySort} title="Sort Visualizer" description="Sort Visualizer is an interactive web application designed to visualize and understand sorting algorithms. Featuring real-time animations, customizable array sizes, gradient bars, and sound effects, it makes learning algorithms engaging and fun." link="https://github.com/namneyugn21/SortVisualizer.git" />
